a1328cd17ad15a3d95669a001fe054543793f37f
Henrique <henrique2204barbosa@gmail.com>
2020-10-13 09:22:31 -0300

    Finalização main

--- campo.py
+++ campo.py
@@ -17,89 +17,29 @@ class E_carga_pontual:
         self.x = x
         self.y = y
 
-        return
 
     def campo_x(self):
+        # Cálculo campo eixo X
         return self.carga_coloumbs*(self.x-self.position[0])/((self.x-self.position[0])**2+(self.y-self.position[1])**2)**(1.5)
 
     def campo_y(self):
+        # Cálculo campo eixo Y
         return self.carga_coloumbs*(self.y-self.position[1])/((self.x-self.position[0])**2+(self.y-self.position[1])**2)**(1.5)
 
     def all_campos(self):
+        # Retorno do valor do campo no eixo X e Y
         return (self.campo_x(), self.campo_y())
 
 # Campo Eletrico total
-def E_total(x,y,cargas):
-    Ex, Ey=0, 0
-    for C in cargas:
-        E = E_carga_pontual(C.load, C.position, x, y).all_campos()
-        Ex=Ex+E[0]
-        Ey=Ey+E[1]
-    return [Ex, Ey]
+class E_total:
+    # Superposição dos valores indivíduais dos campos em um campo total
+    def __init__(self, x,y,cargas):
+        self.Ex, self.Ey=0, 0
+        for C in cargas:
+            E = E_carga_pontual(C.load, C.position, x, y).all_campos()
+            self.Ex=self.Ex+E[0]
+            self.Ey=self.Ey+E[1]
 
-
-class CampoVetorial:
-    """
-        Objeto Singleton, armazenando os campos vetoriais especificos
-        Definição de constantes
-    """
-    __instance = None
-    __constants = {
-        'vacuo': 9E9,
-        'ar': 8.995E9,
-        'agua': 0.11E9,
-        'borracha': 3E9,
-        'enxofre': 2.25E9,
-        'quartzo': 1.8E9,
-        'vidro': 1.5E9,
-        'marmore': 1.125E9,
-        'etanol': 0.36E9,
-        'metanol': 0.265E9,
-        'glicerina': 0.18E9,
-
-
-    }
-
-    __camps = set()
-
-    def __new__(cls, constant=None):
-        if CampoVetorial.__instance is None:
-            CampoVetorial.__instance = object.__new__(cls)
-        return CampoVetorial.__instance
-
-    def __init__(self, constant = None):
-        if constant is not None:
-            self.constant = CampoVetorial.__constants[constant]
-        else:
-            self.constant = CampoVetorial.__constants['vacuo']
-
-    def add_load(cls, camp):
-        cls.__camps.add(camp)
-
-    @classmethod
-    def get_all_loads(cls):
-        return cls.__camps
-
-    @classmethod
-    def get_constant(cls):
-        campo = cls.__instance
-        return campo.constant
-
-    @staticmethod
-    def calculate_in_point(point):
-        CampoVet = 0
-        CampoX = 0
-        CampoY = 0
-        for input in CampoVetorial.get_all_loads():
-            xi, yi = input.position
-            distance = math.sqrt((point.x - xi)**2  + (point.y - yi)**2)
-
-            if distance == 0:
-                distance = 0.0001 # Valor suficientemente grande para passar do tamanho total
-
-            CampoVet += (CampoVetorial.get_constant()*input.load)/(distance)**2
-            CampoX += CampoVet*(point.x-xi)/(distance)**2
-            CampoY += CampoVet*(point.y-yi)/(distance)**2
-        output = Vetor(Point(point.x, point.y),Point(CampoX, CampoY), CampoVet)
-        print(CampoVet)
-        return output
+    def get_campos_eixos(self):
+        # Retorno dos valores do campo total nos eixos X e Y
+        return (self.Ex, self.Ey)
--- main.py
+++ main.py
@@ -3,20 +3,41 @@ from campo import *
 import matplotlib.pylab as plt
 import numpy as np
 
-cargas = [Carga(1, [-1, 0]), Carga(-1, [1, 0])]
+cargas = []
 
-x0, x1=-2, 2
-y0, y1=-1.5, 1.5
+print("Vamos começar a simulação\n")
+
+
+#Adicionamos as cargas deixadas pelo Usuário
+while True:
+    carga = int(input("Digite quantos coloumbs possui a carga\n"))
+    pos_x = int(input("Digite a posição no eixo X da carga\n"))
+    pos_y = int(input("Digite a posição no eixo Y da carga\n"))
+    cargas.append(Carga(carga, (pos_x, pos_y)))
+    keep = input("Deseja adicionar outra carga? (s, n)\n")
+    if keep == "n":
+        break
+
+"""
+    As posições do gráfico são referentes as posiçoes das cargas deixadas pelo usuário
+    O mínimo no eixo x é o valor mínimo que uma carga se situa no eixo x menos 1, assim como no eixo y
+    O mesmo se aplica para o valor máximo para ambos os eixos
+"""
+x0, x1=min([c.position[0] for c in cargas]) - 1, max([c.position[0] for c in cargas]) + 1
+y0, y1=min([c.position[1] for c in cargas]) - 1, max([c.position[1] for c in cargas]) + 1
+
+# Funções referentes à criação de matrizes para as pposições de linhas de campo
 x=np.linspace(x0, x1, 64)
 y=np.linspace(y0, y1, 64)
 x, y=np.meshgrid(x, y)
-Ex, Ey=E_total(x, y, cargas)
-# linhas de campo usando streamplot
+
+# Cálculo das cargas e plotagem no gráfico
+Ex, Ey = E_total(x, y, cargas).get_campos_eixos()
 color = 2 * np.log(np.hypot(Ex, Ey))
-plt.streamplot(x,y,Ex,Ey,color=color, linewidth=1, cmap=plt.cm.inferno,)
+plt.streamplot(x,y,Ex,Ey,color=color, linewidth=1, cmap=plt.cm.inferno)
 
 
-#cargas pontuais
+# Posicionamento das cargas pontuais no Gráfico
 for C in cargas:
     if C.load>0:
         plt.plot(C.position[0],C.position[1],'bo',ms=8*np.sqrt(C.load))
